79540fac983d9c1626730bc4a1b0433d258e75ec
hoooooojjjj <rhj080471@gmail.com>
2023-09-08 19:36:50 +0900

    로그아웃 구현

--- src/page/Welcome.js
+++ src/page/Welcome.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import Card from "../components/Card";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import { userIsLoginContext } from "../App";
+import { signOut } from "firebase/auth";
+import { auth } from "../Myfirebase";
 const Welcome = () => {
   const nav = useNavigate();
+  const islogin = useContext(userIsLoginContext);
   return (
     <div className="Welcome">
       <header>
         <Button
           onClick={() => {
-            nav("/signin");
+            if (islogin) {
+              if (window.confirm("로그아웃 하시겠습니까?")) {
+                signOut(auth);
+              }
+            } else {
+              nav("/signin");
+            }
           }}
           id="Welcome_btn"
           variant="contained"
         >
-          로그인
+          {islogin ? "로그아웃" : "로그인"}
         </Button>
       </header>
       <main>
